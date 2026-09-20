9f9f8cc6fb59d3ff2fe58d87636f550ef8277610
tarekabuozaid <tarekabuzaidcafm@outlook.com>
2026-09-21 03:48:03 +0400

    Refresh cache for decorative Azkaar icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='azkaar-v12';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./qr.png'];
+const CACHE='azkaar-v13';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.webp','./icon-512.webp','./apple-touch-icon.png','./qr.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
