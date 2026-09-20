7594524771c9a187d815d6245b7fcef1b1211b2a
tarekabuozaid <tarekabuzaidcafm@outlook.com>
2026-09-21 03:27:00 +0400

    Refresh cache for smart install UX

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-const CACHE='azkaar-v10';
+const CACHE='azkaar-v11';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./qr.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+  )));
   self.clients.claim();
 });
-
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
-
   if(event.request.mode==='navigate'){
     event.respondWith(
       fetch(event.request).then(response=>{
@@ -28,7 +23,6 @@ self.addEventListener('fetch',event=>{
     );
     return;
   }
-
   event.respondWith(
     caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
       const copy=response.clone();
